29c14b2708498adada17555c6af3c17338bcd556
supermegageil <kimyeojin133@gmail.com>
2022-01-19 10:38:11 +0900

    footer style nice

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,10 +5,11 @@ function Footer(){
     <footer>
         <div className="footer__inner">
             <div className="footer__top">
-                <h1><a href="#"><img src={path+"/img/f01.png"} /></a></h1>
-                <div className="inner">
+                <h1><a href="#"><i class="fas far fa-circle"></i></a>REACT PORTFOLIO</h1>
+                <div className="footer__top__inner">
 
                 <ul className="footer__menu">
+                    <li>Menu</li>
                     <li>Department</li>
                     <li>Gallery</li>
                     <li>Youtube</li>
@@ -18,47 +19,17 @@ function Footer(){
                 </ul>
 
                 <ul className="footer__adress">
-                    <li>VISIT US</li>
+                    <li>Adress</li>
                     <li>React Portfolio</li>
                     <li>Hamburger Allee 249</li>
                     <li>D-98533 Berlin</li>
                     <li>Germany</li>
                     <li>+49 15277 89754</li>
-                    <li><a href="#">mail@reactportfolio.com</a></li>
+                    <li>mail@reactportfolio.com</li>
                 </ul>
-    
-                <div className="footer__newsletter">
-                <form>
-                    <fieldset>
-                    <legend className='h'>뉴스레터 이메일 입력 폼 양식</legend>
-    
-                    <table>
-                        <caption className='h'>뉴스레터 이메일 입력</caption>
-                        <tbody>
-                        <tr>
-                            <th scope='row'>
-                            <label htmlFor="email">Newletter</label>
-                            <p>Newsletter abonnieren</p>
-                            </th>
-                            <td>
-                            <input 
-                                type="text" 
-                                id='email'
-                                name='email'
-                                placeholder='Email'         
-                                />
-                                <button>Los!</button>
-                            </td>
-                            </tr>
-                        </tbody>       
-                        </table>
-                    </fieldset>
-                    </form>
-                    </div>
-                    </div>
                 </div>
+
                 <div className="footer__under">
-        
                     <div className="footer__under__left">
                     <p>Copyright © 2022 React Porfolio. Alle Rechte vorbehalten</p>
                     <p>React Portfolio sowie das Logo sind eingetragene Handelsmarken</p>
@@ -74,10 +45,12 @@ function Footer(){
                     </ul>
                     </div>
                 </div>
-                </div>
-            </footer>
-            )
-        }
+            </div>
+        </div>
+    </footer>
+    )
+}
+    
 
 
 export default Footer;
