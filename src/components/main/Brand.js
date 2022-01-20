71c8d22d1fe6e2f0900a1e030f3177ddb9f19a10
supermegageil <kimyeojin133@gmail.com>
2022-01-20 16:18:22 +0900

    brand nice

--- src/components/main/Brand.js
+++ src/components/main/Brand.js
@@ -2,30 +2,40 @@ const path = process.env.PUBLIC_URL;
 
 function Brand(){
     return(
-        <section id="brand">
-            <div class="brand__inner">
-                <h1>LOREM IPSUM</h1>
-                <div class="brand__wrap">
-                    <article
-                        class="brand__txt">
-                            <i class="fas far fa-circle"></i>
-                            <h2>LOREM IPSUM</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora velit tempore magnam ducimus soluta exercitationem.</p>
-                            <a href="#">MORE</a>
-                                        
+    <div id="brand">
+        <div className="brand__inner">
+            < div className="brand__top__txt">
+                <div className="brand__top__txt__right">          
+                    <h1>BRAND STORY</h1>
+                </div> 
+            </ div>
+        
+        
+        
+            <div className="brand__wrap">
+                <div className="brand__txt">
+                    <i className="fas far fa-circle"></i>
+                    <i className="fas far fa-circle"></i>
+                    <i className="fas far fa-circle"></i>
+                    <h2>LOREM IPSUM</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, iusto cum. Eum, illo obcaecati perferendis exercitationem vitae dolor dolore ut dolorem quas aliquam minus saepe totam blanditiis, dignissimos in praesentium ex quia quam voluptas recusandae delectus sed maxime reprehenderit. Perferendis.</p>
+                        
+                </div>
+
+                <div className="brand__pics">
+                    <article className="brand__pic1">
+                        <img src={path+"/img/brand1.jpg"} alt="브랜드 이미지1"/>
                     </article>
-                    <article class="brand__pic1">
-                        <img src={path+"/img/brand.jpeg"} alt="브랜드 이미지1"/>
+                    <article className="brand__pic2">
+                        <img src={path+"/img/brand2.jpg"} alt="브랜드 이미지2"/>
                     </article>
-                    <article class="brand__pic2">
-                        <img src={path+"/img/brand.jpeg"} alt="브랜드 이미지2"/>
-                    </article>
-                    <article class="brand__pic3">
-                        <img src={path+"/img/brand.jpeg"} alt="브랜드 이미지3"/>
+                    <article className="brand__pic3">
+                        <img src={path+"/img/brand3.jpg"} alt="브랜드 이미지3"/>
                     </article>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>    
 
     )
 }
