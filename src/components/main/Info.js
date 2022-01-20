de891e703e25acd46b640f27c1df22c84eb276cf
supermegageil <kimyeojin133@gmail.com>
2022-01-20 10:08:49 +0900

    info top typo pic size

--- src/components/main/Info.js
+++ src/components/main/Info.js
@@ -6,14 +6,18 @@ function Info(){
   return (
    <div id="info">
       <div className="info__inner">
-        <h1>PROJECTS</h1>
-        <div class="info__wrap">
-            <article>
-                <div class="pic">
-                <img src={path+"/img/info.jpeg"} />
+            < div class="info__top__txt">
+                <div className="info__top__txt__left">          <h1>PROJECTS</h1>
+                </div> 
+                <div className="info__top__txt__right">
+                <button>lorem</button>
+                <button>lorem</button>
                 </div>
-                <h2>Lorem, ipsum.</h2>
-            </article>
+            </ div>
+            
+         
+            
+            <div class="info__wrap">        
             <article>
                 <div class="pic">
                     <img src={path+"/img/info.jpeg"} />
