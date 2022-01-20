5d77fa2563d391fff21bd8484cf04d27131d3211
supermegageil <kimyeojin133@gmail.com>
2022-01-20 16:31:47 +0900

    banner fertig

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -1,10 +1,7 @@
-
-
-
 function Banner() {
     return (
         <section id="banner">
-            <div class="banner__inner">
+            <div className="banner__inner">
                 <h1>Lorem ipsum dolor sit amet consectetur.</h1>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident ipsa nulla libero perferendis corporis excepturi! Nostrum sequi aut provident voluptatem.</p>
             </div>
